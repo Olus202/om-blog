7bb84c55450bee554ddcaa3f05ca2e2b4c82be8c
Olus202 <aleksandrabacik@gmail.com>
2016-11-14 21:14:28 +0100

    Running home_page with html response

--- blog/tests.py
+++ blog/tests.py
@@ -1,5 +1,6 @@
 from django.core.urlresolvers import resolve
 from django.test import TestCase
+from django.http import HttpRequest
 from blog.views import home_page
 
 
@@ -9,4 +10,10 @@ class HomePageTest(TestCase):
         found = resolve('/')
         self.assertEqual(found.func, home_page)
 
+    def test_home_page_to_html(self):
+        request = HttpRequest()
+        response = home_page(request)
+        self.assertTrue(response.content.startswith(b'<html>'))
+        self.assertIn(b'<title>outdoor marriage</title>', response.content)
+        self.assertTrue(response.content.endswith(b'</html>'))
 
