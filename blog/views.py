ed7680412bd7752b4b54ac4b5fedab40b430444c
Olus202 <aleksandrabacik@gmail.com>
2016-11-26 21:41:28 +0100

    Style - html, css, jquery; new views

--- blog/views.py
+++ blog/views.py
@@ -11,3 +11,14 @@ def home_page(request):
 def post_view(request, pk):
     post = get_object_or_404(Post, pk=pk)
     return render(request, 'post_view.html', {'post': post})
+
+
+def all_posts(request):
+    posts = Post.objects.all()
+    return render(request, 'all_posts.html', {'posts': posts})
+
+
+def polish_crown(request):
+    posts = Post.objects.all()
+    return render(request, 'polish_crown.html', {'posts': posts})
+
--- omblog/urls.py
+++ omblog/urls.py
@@ -4,5 +4,7 @@ from django.contrib import admin
 urlpatterns = [
      url(r'^$', 'blog.views.home_page', name='home'),
      url(r'^post/(?P<pk>[0-9]+)/$', 'blog.views.post_view', name='post_view'),
+     url(r'^all/$', 'blog.views.all_posts', name='all_posts'),
+     url(r'^crown/$', 'blog.views.polish_crown', name='polish_crown'),
      url(r'^admin/', include(admin.site.urls)),
 ]
