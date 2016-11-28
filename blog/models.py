32adb1ed0b6511ec03a6f01eb2be9d34d5f92986
Olus202 <aleksandrabacik@gmail.com>
2016-11-28 14:58:43 +0100

    Adding image field to Post model

--- blog/models.py
+++ blog/models.py
@@ -6,3 +6,4 @@ class Post(models.Model):
     header = models.CharField(max_length=200, default='')
     text = models.TextField(default='')
     pub_date = models.DateField(default=datetime.date.today())
+    image = models.ImageField(upload_to='photos/posts', default='')
